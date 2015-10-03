36f8b37e1fc9353fca780fd194767c18c6ac6528
NancyKo <konancys@gmail.com>
2015-10-03 12:26:04 -0400

    Almost done - 96 percent ARRGGGHHH

--- spec/jukebox_spec.rb
+++ spec/jukebox_spec.rb
@@ -10,9 +10,38 @@ songs = [
 ]
 
 describe Song do
-
+  it "has a name" do
+    song = Song.new(songs[0])
+    expect(song.name).to eq(songs[0])
+  end
 end
 
 describe Jukebox do
+  let(:jukebox) {Jukebox.new(songs)}
 
+  it 'tell you if it is on' do 
+    expect(jukebox.on?).to eq(true)
+  end
+
+  it 'can give help menu' do
+    expect(jukebox.help).to eq("Please select help, list, exit, or play.")
+  end
+
+  it 'can exit out' do
+    jukebox.exit
+    expect(jukebox.on?).to eq(false)
+  end
+
+  it "can list" do
+    expect(jukebox.command("list")).to eq(jukebox.list)
+  end
+
+  it 'can play' do
+    jukebox.play(songs[4])
+    expect(jukebox.command("play")).to eq("now playing \'(Far From)\' Home - Tiga")
+  end
+
+  it "can handle invalid command" do
+    expect(jukebox.command('dkjsl')).to eq("invalid command")
+  end 
 end
